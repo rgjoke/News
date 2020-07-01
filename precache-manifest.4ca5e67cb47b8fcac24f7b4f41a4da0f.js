eb4ec50031f79adc7386c398ddf28e51e8ac4736
rgjoke <r.darktemptation@gmail.com>
2020-07-01 19:15:14 +0600

    Updates

---
+++ precache-manifest.4ca5e67cb47b8fcac24f7b4f41a4da0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30908ca3c051254b147512c8bc3cbe10",
+    "url": "/ololo/index.html"
+  },
+  {
+    "revision": "296d98bfbb04b28668ca",
+    "url": "/ololo/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2acb06b422de1aa2ad36",
+    "url": "/ololo/static/css/main.49161108.chunk.css"
+  },
+  {
+    "revision": "296d98bfbb04b28668ca",
+    "url": "/ololo/static/js/2.fb02ee1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ololo/static/js/2.fb02ee1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2acb06b422de1aa2ad36",
+    "url": "/ololo/static/js/main.0b46e3df.chunk.js"
+  },
+  {
+    "revision": "ced729e65fa06fe77408",
+    "url": "/ololo/static/js/runtime-main.0ed2f40f.js"
+  }
+]);
